ea7a875fd998b645f874a5c3f73455a589eae8b1
scotthartbti <scott.hart.bti@gmail.com>
2013-04-18 23:07:56 -0400

    Mms auto-retrieval 3/3 Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    Change-Id: I0332beccd61d4942c6acb03b434cbebbce39eb80

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -21,6 +21,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -40,6 +41,7 @@ import android.preference.PreferenceScreen;
 import android.preference.RingtonePreference;
 import android.provider.SearchRecentSuggestions;
 import android.text.TextUtils;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -85,6 +87,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     private CheckBoxPreference mEnableNotificationsPref;
     private CheckBoxPreference mMmsAutoRetrievialPref;
     private RingtonePreference mRingtonePref;
+    private CheckBoxPreference mMmsRetrievalDuringRoamingPref;
     private Recycler mSmsRecycler;
     private Recycler mMmsRecycler;
     private static final int CONFIRM_CLEAR_SEARCH_HISTORY_DIALOG = 3;
@@ -121,9 +124,17 @@ public class MessagingPreferenceActivity extends PreferenceActivity
         mMmsLimitPref = findPreference("pref_key_mms_delete_limit");
         mClearHistoryPref = findPreference("pref_key_mms_clear_history");
         mEnableNotificationsPref = (CheckBoxPreference) findPreference(NOTIFICATION_ENABLED);
-        mMmsAutoRetrievialPref = (CheckBoxPreference) findPreference(AUTO_RETRIEVAL);
         mVibratePref = (CheckBoxPreference) findPreference(NOTIFICATION_VIBRATE);
         mRingtonePref = (RingtonePreference) findPreference(NOTIFICATION_RINGTONE);
+
+	// Get the MMS retrieval settings. Defaults to enabled with roaming disabled
+        mMmsAutoRetrievialPref = (CheckBoxPreference) findPreference(AUTO_RETRIEVAL);
+        ContentResolver resolver = getContentResolver();
+        mMmsAutoRetrievialPref.setChecked(Settings.System.getInt(resolver,
+                Settings.System.MMS_AUTO_RETRIEVAL, 1) == 1);
+        mMmsRetrievalDuringRoamingPref = (CheckBoxPreference) findPreference(RETRIEVAL_DURING_ROAMING);
+        mMmsRetrievalDuringRoamingPref.setChecked(Settings.System.getInt(resolver,
+                Settings.System.MMS_AUTO_RETRIEVAL_ON_ROAMING, 0) == 1);
 
         setMessagePreferences();
     }
@@ -284,8 +295,16 @@ public class MessagingPreferenceActivity extends PreferenceActivity
         } else if (preference == mEnableNotificationsPref) {
             // Update the actual "enable notifications" value that is stored in secure settings.
             enableNotifications(mEnableNotificationsPref.isChecked(), this);
+	} else if (preference == mMmsRetrievalDuringRoamingPref) {
+            // Update the value in Settings.System
+            Settings.System.putInt(getContentResolver(), Settings.System.MMS_AUTO_RETRIEVAL_ON_ROAMING,
+                    mMmsRetrievalDuringRoamingPref.isChecked() ? 1 : 0);
         } else if (preference == mMmsAutoRetrievialPref) {
-            if (mMmsAutoRetrievialPref.isChecked()) {
+            // Update the value in Settings.System
+            boolean checked = mMmsAutoRetrievialPref.isChecked();
+            Settings.System.putInt(getContentResolver(), Settings.System.MMS_AUTO_RETRIEVAL,
+                    checked ? 1 : 0);
+            if (checked) {
                 startMmsDownload();
             }
         }
